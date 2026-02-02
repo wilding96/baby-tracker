6cbc6b9e4f10b244cb23949bbe953b1c0e2040b0
Dylan <wilding96@qq.com>
2026-02-02 17:02:14 +0800

    feat: added shadcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import homepageBg from "@/static/image/homepage_bg.png";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -9,8 +10,7 @@ export default function Home() {
           className="dark:invert"
           src={homepageBg}
           alt="bg"
-          width={300}
-          height={300}
+          placeholder="blur"
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
@@ -19,21 +19,7 @@ export default function Home() {
           </h1>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Read Now
-          </a>
+          <Button className="bg-green-600 hover:bg-green-700">Read Now</Button>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="#"
